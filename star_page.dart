bf8b82c992c1dd5fe8e5674de6713251ba058c91
dhiman-RK <101541366+dhiman-RK@users.noreply.github.com>
2023-06-20 18:31:37 +0530

    Add files via upload

---
+++ star_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StarPage extends StatefulWidget {
+  const StarPage({Key? key}) : super(key: key);
+
+  @override
+  _StarPageState createState() => _StarPageState();
+}
+
+class _StarPageState extends State<StarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
